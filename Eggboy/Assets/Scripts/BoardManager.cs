77b53def69e6f485143aeacc2d0ad520c5413549
Hugo Bombail <hugo.bombail@wanadoo.fr>
2016-03-24 14:25:37 -0400

    On repart sur de bonnes bases pour le nouveau concept : Deplacement sur des cases, espace pour passer un tour.

---
+++ Eggboy/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count ( int min, int max ) 
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 15;
+	public int rows = 8;
+
+	public GameObject[] floorTiles;
+	public GameObject[] objectTiles;
+
+
+
+	private Transform boardHolder;
+	//boardHolder permet lorsqu'on fait spawn plein de trucs de garder la hierarchie clean
+	private List <Vector3> gridPositions = new List<Vector3>();
+
+	void InitialiseList(){
+		gridPositions.Clear ();
+		for (int x =0; x < columns; x++) {
+			for(int y = 0; y < rows; y++){
+				gridPositions.Add(new Vector3(x, y, 0f));
+			}
+		}
+	}
+
+	void boardSetup(){
+		boardHolder = new GameObject ("Board").transform;
+		for (int x = 0; x < columns; x++) {
+			for(int y = 0; y < rows; y++){
+				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+				// on instancie un floortiles choisi en random dans la liste des floortiles
+				GameObject instantiate = Instantiate(toInstantiate, new Vector3(x,y,0f),Quaternion.identity ) as GameObject;
+				// on définie un gameObject qui va nous permettre d'instancier les objets choisis au préalable par toInstantiate
+				instantiate.transform.SetParent(boardHolder);
+				// boardHolder est parent ( dans la hierarchie ) des objets instanciés 
+			}
+		}
+	}
+
+	Vector3 RandomPosition(){
+	// determine une position Random parmis la grille gridPosition
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
+	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum){
+		int objectCount = Random.Range (minimum, maximum+1);
+		for (int i = 0; i < objectCount; i++) {
+			Vector3 randomPosition = RandomPosition();
+			GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+			Instantiate(tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+
+	public void SetupScene(int level){
+		boardSetup ();
+		InitialiseList ();
+		int enemyCount = level;
+		LayoutObjectAtRandom (objectTiles, enemyCount, enemyCount);
+	}
+}
---
+++ Eggboy/Assets/Scripts/Player.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+
+	public float restartLevelDelay = 1f;
+	
+	private Animator animator;
+
+	private int hp;
+	private GameManager manager;
+
+	protected override void Start () {
+		manager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+		animator = GetComponent<Animator> ();
+		hp = GameManager.instance.playerhpPoints;
+		base.Start ();
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		base.AttemptMove<T> (xDir, yDir);
+		RaycastHit2D hit;
+		CheckIfGameOver ();
+		GameManager.instance.playersTurn = false;
+		Debug.Log (hp.ToString ());
+	}
+
+	private void onDisable(){
+		GameManager.instance.playerhpPoints = hp;
+	}
+
+	private void CheckIfGameOver(){
+		if (hp <= 0) {
+			GameManager.instance.GameOver();
+		}
+	}
+
+	protected override void OnCantMove<T> (T component)
+	{
+		if (component.tag == "Wall")
+			return;
+		else if(component.tag == "Enemy"){
+			return;
+		}
+	}
+
+	private void Restart(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void loseHP(){
+		hp -= 1;
+		CheckIfGameOver ();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Exit") {
+			//Invoke permet de mettre un delay avant de charger une fonction, ici Restart()
+			Invoke ("Restart", restartLevelDelay);
+			//==desactive le joueur, je pense
+			enabled = false;
+		} 
+	}
+
+	void Update () {
+		if (!manager.playersTurn)
+		//permet de ne pas se préoccuper de update si ce n'est pas le tour du joueur
+			return;
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int) Input.GetAxisRaw ("Horizontal");
+		vertical = (int)Input.GetAxisRaw ("Vertical");
+
+		if (horizontal != 0){
+		// pour empecher le mouvement en diagonale
+			vertical = 0;
+			switch (horizontal) {
+			case -1:
+				animator.SetBool ("isRight", false);
+				break;
+			case 1:
+				animator.SetBool ("isRight", true);
+				break;
+			}
+		}
+		if (horizontal != 0 || vertical != 0) {
+			AttemptMove<Wall>(horizontal, vertical);
+		}
+		else if(Input.GetKeyDown(KeyCode.Space)){
+			manager.playersTurn = false;
+		}
+	
+	}
+}
