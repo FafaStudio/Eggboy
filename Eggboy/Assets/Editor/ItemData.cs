3a420f985736c643e82eb3fb7de9619df5c9f45a
DESKTOP-5FDJABE\sulex <sulexa95@gmail.com>
2016-10-12 19:12:43 +0200

    Ajoute de la catégorie Chests

--- Eggboy/Assets/Editor/ItemData.cs
+++ Eggboy/Assets/Editor/ItemData.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 [System.Serializable]
 public class Item
 {
-    public enum TestEnum { Mobs, Traps, Mobiliers };
+    public enum TestEnum { Mobs, Traps, Mobiliers, Chests };
     public string Name;
     public TestEnum CategorieName;
     public GameObject Prefab;
