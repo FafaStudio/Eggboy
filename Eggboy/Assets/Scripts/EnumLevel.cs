c31eaf2718335dd781d35779f7c8a8349a249bd3
Bicephal <claissejean@gmail.com>
2016-10-19 05:46:28 +0200

    - Ajout de 7 niveaux supplémentaires (jusqu'au 50) - Implementation des recompenses pour chaque ennemi - Rectification du niveau 36 - Implémentation du nombre de tours optimal pour chaque PUTAIN de niveau - Implémentation d'un variable "Difficulté" sur l'objet levelDesign qui va de "Tuto" à "Infame" - Implémentation des variables difficultés pour chacun des putains de niveau nuls - Google drive à jour - J'me la coupe, et j'me la bouffe en direct aux 35h de direct d'Hanouna

--- Eggboy/Assets/Scripts/EnumLevel.cs
+++ Eggboy/Assets/Scripts/EnumLevel.cs
@@ -3,4 +3,6 @@ using System.Collections;
 
 public class EnumLevel : MonoBehaviour {
     public int nbTourOpti;
+    public enum Difficulte {tuto, facile, difficile, infame};
+    public Difficulte dir;
 }
