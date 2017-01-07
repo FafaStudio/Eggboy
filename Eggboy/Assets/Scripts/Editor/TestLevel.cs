4e9b9c7fdaa0ed11dda8123a881b577d7f24b242
DESKTOP-5FDJABE\sulex <sulexa95@gmail.com>
2017-01-07 18:51:05 +0100

    Ajout du simplifiage de vie pour juan Correction d'une erreur lors d'un mouvement vers un mur

---
+++ Eggboy/Assets/Scripts/Editor/TestLevel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class TestLevel : MonoBehaviour {
+
+    static TestLevel()
+    {
+        EditorApplication.update += Update;
+    }
+    static string originalScene;
+
+    public static void BeginLevel()
+    {
+        originalScene = EditorSceneManager.GetActiveScene().name;
+        PlayerPrefs.SetInt("level", int.Parse(originalScene.Substring(5)));
+
+        SceneManager.LoadScene("BeginingLevel", LoadSceneMode.Single);
+
+        var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+    }
+
+
+    static bool state = false;
+
+    static void Update()
+    {
+        if(EditorApplication.isPlaying != state)
+        {
+            state = EditorApplication.isPlaying;
+            if (state)
+            {
+                if (EditorSceneManager.GetActiveScene().name.IndexOf("Level") == 0)
+                {
+                    BeginLevel();
+                }
+            }
+            else
+            {
+                PlayerPrefs.SetInt("level", -1);
+            }
+        }
+    }
+
+}
